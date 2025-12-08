d6add13cdeac5f22a5452c82e326a9e60a3b72b1
danielerazo9 <derazo033@gmail.com>
2025-12-07 23:57:24 -0800

    feat: add Milestone 3A project folder

---
+++ bootcamp-milestone-3A/src/components/blogPreview.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./blogPreview.module.css";
+
+export type BlogPreviewProps = {
+  slug: string;
+  title: string;
+  date: string;
+  image: string;
+  content: string;
+};
+
+export default function BlogPreview({ slug, title, date, image, content }: BlogPreviewProps) {
+    const formattedDate =
+    typeof date === "string"
+      ? date
+      : new Date(date).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+
+  return (
+    <div className={styles.card}>
+      <Image src={image} alt={title} width={500} height={300} className={styles.image} />
+      <h3 className={styles.title}>
+        <Link href={`/blog/${slug}`}>{title}</Link>
+      </h3>
+      <p className={styles.meta}><strong>Posted:</strong> {formattedDate}</p>
+      <p className={styles.text}>{content}</p>
+      <Link className={styles.readMore} href={`/blog/${slug}`}>Read more →</Link>
+    </div>
+  );
+}
+
+
